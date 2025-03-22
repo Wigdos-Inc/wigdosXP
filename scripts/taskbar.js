11b9b9fd3bbce1ab4e023d88396592033eceec17
stu102871 <102871@glr.nl>
2025-03-22 15:23:00 +0000

    Taskbar Time Display

---
+++ scripts/taskbar.js
@@ -0,0 +1,26 @@
+const timeBox = document.getElementById("tbTimeBox");
+
+
+
+// Time Stuff
+const baseClock = setInterval(() => update("tbClock"), 1000);
+
+function update(type) {
+
+    // Store the Current Time
+    const cDate = new Date;
+    const cTime = {
+        hours  : (cDate.getHours() < 10 ? "0" : "") + cDate.getHours(),
+        minutes: (cDate.getMinutes() < 10 ? "0" : "") + cDate.getMinutes()
+    }
+
+    if (type == "tbClock") timeBox.innerHTML = `${cTime.hours}:${cTime.minutes}`;
+}
+update("tbClock");
+
+
+
+timeBox.addEventListener("click", () => {
+
+    // Expanded Time Stuff
+});
