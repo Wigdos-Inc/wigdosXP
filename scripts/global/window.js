42387398940a32029aff7b99e4f69a39157ee682
stu102871 <102871@glr.nl>
2025-03-24 09:17:49 +0000

    abba

--- scripts/global/window.js
+++ scripts/global/window.js
@@ -11,7 +11,9 @@ class AppWindow {
         this.focus   = false;
         this.cClose  = undefined;
         this.index   = windows.index;
-        this.nameBox = undefined
+        this.nameBox = undefined;
+        this.cMove   = undefined;
+        this.full    = false;
     }
 
     // Create the Application Window
@@ -34,8 +36,27 @@ class AppWindow {
         const selectBox = appHeader.appendChild(document.createElement("div"));
         selectBox.classList.add("selectBox");
 
+
+        const minBtn = selectBox.appendChild(document.createElement("div"));
+        minBtn.classList.add("appMin", "selectBtns");
+        minBtn.innerHTML = "<strong>_</strong>";
+
+        minBtn.onclick = () => this.close();
+
+
+        const screenBtn = selectBox.appendChild(document.createElement("div"));
+        screenBtn.classList.add("appScreen", "selectBtns");
+        screenBtn.innerHTML = "<strong>W</strong>";
+
+        screenBtn.onclick = () => {
+            
+            this.full = !this.full;
+            this.screenChange();
+        }
+
+
         const closeBtn = selectBox.appendChild(document.createElement("div"));
-        closeBtn.classList.add("appClose");
+        closeBtn.classList.add("appClose", "selectBtns");
         closeBtn.innerHTML = "<strong>X</strong>";
 
         closeBtn.onclick = () => this.close();
@@ -71,26 +92,55 @@ class AppWindow {
         }
         else location.href = "../../index.html";
     }
+
+    screenChange() {
+
+        if (this.full) {
+
+            this.element.style.left = 0;
+            this.element.style.top = 0;
+
+            this.element.style.height = "100%";
+            this.element.style.width = "100%";
+
+        } else {
+
+            this.element.style.left = "50%";
+            this.element.style.top = "50%";
+            this.element.style.transform = 
+
+            this.element.style.height = "400px";
+            this.element.style.width = "600px";
+
+        }
+    }
 }
 
 
 // Handle Application Loading
 function application(type) {
 
+    const application = new AppWindow();
+    let appTitle;
+
     switch (type) {
 
         case "files": 
         
-            const files = new AppWindow();
-            files.cClose = true;
-            files.create();
+            application.cClose = true;
+            application.cMove = true;
+            application.full = false;
 
-            files.nameBox.innerHTML = "File Explorer";
-
-            windows.object.push(files);
+            appTitle = "File Explorer";
             
             break;
     }
+    
+    application.create();
+    application.nameBox.innerHTML = appTitle;
 
+    application.screenChange();
+
+    windows.object.push(application);
     windows.index++;
 }
