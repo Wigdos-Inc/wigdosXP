14058172d689dc2365aa0caaef24aca58113f0ed
stu102871 <102871@glr.nl>
2025-03-25 19:43:39 +0000

    Applications Progress

--- scripts/global/window.js
+++ scripts/global/window.js
@@ -12,11 +12,23 @@ class AppWindow {
         this.cClose  = cClose;
         this.index   = windows.index;
         this.nameBox = undefined;
-        this.cMove   = cMove;
         this.full    = full;
 
         this.header  = undefined;
         this.content = undefined;
+
+        this.move    = {
+            able   : cMove,
+            current: false,
+            xOffset: 0,
+            yOffset: 0,
+            storage: {
+                x: undefined,
+                y: undefined,
+                h: 400,
+                w: 600
+            }
+        }
     }
 
     // Create the Application Window
@@ -77,9 +89,13 @@ class AppWindow {
         appMain.classList.add("appMain");
 
 
-        // Top Bar (Content Depends on Application)
+        // Top Bar
         this.header = appMain.appendChild(document.createElement("div"));
         this.header.classList.add("appMainTopBar");
+
+        const os = this.header.appendChild(document.createElement("img"));
+        os.classList.add("appOS");
+        os.src = "../../assets/images/icons/16x/creature.png";
 
 
         // Mid Bar (Content Depends on Application)
@@ -90,6 +106,56 @@ class AppWindow {
         // Bottom Bar
         const bottomBar = appMain.appendChild(document.createElement("div"));
         bottomBar.classList.add("appMainBottomBar");
+
+
+
+        // Drag Functionality
+
+        if (this.move.able) {
+
+            appHeader.addEventListener("mousedown", (event) => {
+
+                if (
+                    minBtn.contains(event.target) ||
+                    screenBtn.contains(event.target) || 
+                    closeBtn.contains(event.target)
+                ) {return} else {
+
+                    this.move.current = true;
+
+                    this.move.xOffset = event.clientX - this.element.offsetLeft;
+                    this.move.yOffset = event.clientY - this.element.offsetTop;
+                    
+                    this.element.style.transition = "none";
+
+                }
+            });
+
+            document.addEventListener("mousemove", (event) => {
+
+                if (this.move.current) {
+                    this.element.style.left = `${event.clientX - this.move.xOffset}px`;
+                    this.element.style.top = `${event.clientY - this.move.yOffset}px`;
+
+                    // Requires Debugging
+                    //this.element.style.width = `${this.move.storage.w}px`;
+                    //this.element.style.height = `${this.move.storage.h}px`;
+                }
+            });
+    
+            document.addEventListener("mouseup", () => {
+
+                if (this.move.current) {
+                    this.move.current = false;
+                    this.element.style.transition = "all 0.1s";
+
+                    const pos = this.element.getBoundingClientRect();
+                    this.move.storage.x = pos.x;
+                    this.move.storage.y = pos.y;
+                }
+            });
+
+        }
     }
 
     // Close the Application Window
@@ -116,17 +182,31 @@ class AppWindow {
             this.element.style.borderTopLeftRadius = 0;
             this.element.style.borderTopRightRadius = 0;
 
+            this.move.able = false;
+
         } else {
 
-            this.element.style.left = "50%";
-            this.element.style.top = "50%";
-            this.element.style.transform = "translate(-50%, -50%)";
+            if (this.move.storage.x) {
 
-            this.element.style.height = "400px";
-            this.element.style.width = "600px";
+                this.element.style.left = `${this.move.storage.x}px`;
+                this.element.style.top  = `${this.move.storage.y}px`;
+
+            } else {
+
+                this.element.style.left = "50%";
+                this.element.style.top = "50%";
+                this.element.style.transform = "translate(-50%, -50%)";
+
+            }
+            
+
+            this.element.style.height = `${this.move.storage.h}px`;
+            this.element.style.width = `${this.move.storage.w}px`;
 
             this.element.style.borderTopLeftRadius = "5px";
             this.element.style.borderTopRightRadius = "5px";
+
+            this.move.able = true;
 
         }
     }
@@ -141,16 +221,18 @@ function application(type) {
     let full;
     let appTitle;
 
+    let appender = document.createElement("div");
+
     switch (type) {
 
         case "files": 
 
-            app = new AppWindow(true, true, false);
             cClose = true;
             cMove = true;
             full = false;
-        
             appTitle = "File Explorer";
+
+            appender = files(appender);
         
             break;
 
@@ -159,8 +241,9 @@ function application(type) {
             cClose = false;
             cMove = false;
             full = true;
-
             appTitle = "WiggleSearch";
+
+            appender = null;
         
         break;
 
@@ -169,18 +252,60 @@ function application(type) {
             cClose = true;
             cMove = true;
             full = false;
+            appTitle = "Notepad";
 
-            appTitle = "Notepad"
+            appender = notes(appender);
 
             break;
+
+        case "bin":
+    
+            cClose = true;
+            cMove = true;
+            full = false;
+            appTitle = "Recycling Bin";
+
+            appender = bin(appender);
+    
+            break;
+
+        default: return;
     }
     
     const application = new AppWindow(cClose, cMove, full)
     application.create();
     application.nameBox.innerHTML = appTitle;
-
     application.screenChange();
+
+    if (appender) {
+        appender.classList.add("appender");
+        application.content.appendChild(appender);
+    }
 
     windows.object.push(application);
     windows.index++;
+}
+
+
+
+function files(appContentBox) {
+
+    appContentBox.classList.add("filesBox");
+
+    for (let i=0; i < 5; i++) {
+
+        const filesItem = appContentBox.appendChild(document.createElement("div"));
+        filesItem.classList.add("filesItem");
+    }
+
+    return appContentBox;
+}
+
+function notes() {
+
+    return null;
+}
+function bin() {
+
+    return null;
 }
