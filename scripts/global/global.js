bd90867364e69dff92d01b37e9788e6d2c17a205
Tayrin GLR <102871@glr.nl>
2025-06-24 19:09:07 +0000

    Added Default Placeholder IMG

--- scripts/global/global.js
+++ scripts/global/global.js
@@ -23,3 +23,16 @@ function getUser() {
 
     return localStorage.getItem("username");
 }
+
+
+
+const imgs = document.getElementsByTagName("img");
+for (let i=0; i < imgs.length; i++) {
+
+    let size;
+    if (imgs[i].offsetWidth < 100) size = "16x";
+    else if (imgs[i].offsetWidth < 200) size = "32x";
+    else size = "48x";
+
+    imgs[i].src = `assets/images/icons/${size}/bombs.png`;
+}
